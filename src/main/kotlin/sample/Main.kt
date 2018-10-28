40643073a0b0f1a7b361ed10632ab2c27d0bb025
Paul Adam <pavlosadam@gmail.com>
2018-10-28 22:44:29 +0200

    Added basic ktor implemntarion.

---
+++ src/main/kotlin/sample/Main.kt
@@ -0,0 +1,25 @@
+package sample
+
+import io.ktor.application.*
+import io.ktor.http.*
+import io.ktor.response.*
+import io.ktor.routing.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+
+fun main(args: Array<String>) {
+    val server = embeddedServer(Netty, port = 8080) {
+        routing {
+            get(path = "/") {
+                call.respondText("Hello There.", ContentType.Text.Plain)
+            }
+            get(path = "/demo") {
+                call.respondText("General Kenobi!")
+            }
+            get(path = "potato") {
+                call.respondText("potato")
+            }
+        }
+    }
+    server.start(wait = true)
+}
