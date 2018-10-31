373f12b4fa8f631f9dd89e6f811a4ec4424ff2d4
Paul Adam <pavlosadam@gmail.com>
2018-10-31 22:27:03 +0200

    Respond with a simple HTML page on the potato API call.

--- src/main/kotlin/sample/Main.kt
+++ src/main/kotlin/sample/Main.kt
@@ -1,11 +1,13 @@
 package sample
 
 import io.ktor.application.*
+import io.ktor.html.respondHtml
 import io.ktor.http.*
 import io.ktor.response.*
 import io.ktor.routing.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
+import kotlinx.html.*
 
 fun main(args: Array<String>) {
     val server = embeddedServer(Netty, port = 8080) {
@@ -17,7 +19,23 @@ fun main(args: Array<String>) {
                 call.respondText("General Kenobi!")
             }
             get(path = "/potato") {
-                call.respondText("potato")
+                call.respondHtml {
+                    head {
+                        title { +"this is the page title" }
+                    }
+                    body {
+                        p { +"this is a paragraph" }
+                        p { +"this is another paragraph" }
+                        p {
+                            + "this paragraph has an input field for some reason ==> "
+                            input{
+                                value = "default value"
+                                name = "randomInput"
+                                autoFocus = true
+                            }
+                        }
+                    }
+                }
             }
         }
     }
